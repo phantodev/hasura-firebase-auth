b4fe5988419d920152aee5c138b208f63a91a54a
phantoxe <eduardo@dotmidia.com.br>
2021-11-26 14:37:13 -0300

    Add Hasura Codegen

--- pages/api/signup.js
+++ pages/api/signup.js
@@ -1,5 +1,53 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+// export default function handler(req, res) {
+//   res.status(200).json({ name: 'John Doe' })
+// }
+
+const fetch = require("node-fetch");
+
+const HASURA_OPERATION = `
+mutation ($email: String!, $state: String!, $city: String!) {
+  insert_desiree_pre_signup_one(object: {email: $email, state: $state, city: $city}) {
+    id
+  }
 }
+`;
+
+// execute the parent operation in Hasura
+const execute = async (variables) => {
+  const fetchResponse = await fetch(
+    "https://desiree-pwa-app.herokuapp.com/v1/graphql",
+    {
+      method: "POST",
+      body: JSON.stringify({
+        query: HASURA_OPERATION,
+        variables,
+      }),
+    }
+  );
+  const data = await fetchResponse.json();
+  console.log("DEBUG: ", data);
+  return data;
+};
+
+// Request Handler
+app.post("/signup", async (req, res) => {
+  // get request input
+  const { email, state, city } = req.body.input;
+
+  // run some business logic
+
+  // execute the Hasura operation
+  const { data, errors } = await execute({ email, state, city });
+
+  // if Hasura operation errors, then throw error
+  if (errors) {
+    return res.status(400).json(errors[0]);
+  }
+
+  // success
+  return res.json({
+    ...data.insert_desiree_pre_signup_one,
+  });
+});
